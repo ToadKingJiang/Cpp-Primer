942bbf8272518018b5c8562dbd11912580e48ac9
shizumik <497088711@qq.com>
2016-10-13 16:02:27 +0800

    Update ex3_10.cpp
     there is only one output a .

--- ch03/ex3_10.cpp
+++ ch03/ex3_10.cpp
@@ -12,8 +12,9 @@ int main()
 {
     cout << "Enter a string of characters including punctuation." << endl;
     for (string s; getline(cin, s); cout << endl)
-        for (auto i : s) 
+        for (auto i : s)
+        {    
             if (!ispunct(i)) cout << i;
-
+        }
     return 0;
 }
